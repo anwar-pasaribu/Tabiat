6ad75bef60e0109aedb509db771da38565556301
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-05-30 01:18:33 +0700

    fix - remove unnecessary code from MainViewController.kt

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,16 +1,10 @@
-import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.ui.window.ComposeUIViewController
 import di.letsKoinStart
 import platform.UIKit.UIViewController
 
-@OptIn(ExperimentalComposeApi::class)
 fun MainViewController(): UIViewController {
     letsKoinStart()
-    return ComposeUIViewController(
-        configure = {
-            platformLayers = true
-        }
-    ) {
+    return ComposeUIViewController{
         App()
     }
 }
