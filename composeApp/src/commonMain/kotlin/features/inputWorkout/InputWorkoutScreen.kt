0dd2012f410934aefb15998c7fa8e0da62401e50
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-05-29 17:19:43 +0700

    improve - input workout plan UI, hide optional field

--- composeApp/src/commonMain/kotlin/features/inputWorkout/InputWorkoutScreen.kt
+++ composeApp/src/commonMain/kotlin/features/inputWorkout/InputWorkoutScreen.kt
@@ -1,19 +1,29 @@
 package features.inputWorkout
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -27,7 +37,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.LayoutDirection
@@ -86,7 +98,7 @@ fun InputWorkoutScreen(
         },
     ) { contentPadding ->
 
-        Card (
+        Card(
             modifier = Modifier.fillMaxWidth().padding(
                 start = contentPadding.calculateStartPadding(LayoutDirection.Ltr) + 8.dp,
                 top = contentPadding.calculateTopPadding() + 16.dp,
@@ -100,36 +112,69 @@ fun InputWorkoutScreen(
                 var workoutName by remember { mutableStateOf(workoutPlan?.name.orEmpty()) }
                 var workoutNotes by remember { mutableStateOf(workoutPlan?.description.orEmpty()) }
 
+                val workoutPlanInputTextStyle = MaterialTheme.typography.titleLarge
                 Spacer(Modifier.height(16.dp))
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                    textStyle = workoutPlanInputTextStyle,
                     value = workoutName,
                     onValueChange = {
                         workoutName = it
                     },
-                    label = { Text("Nama Sesi Workout") },
-                    placeholder = { Text("Ex. Latihan Punggung") },
+                    supportingText = { Text("Plan workout misal: Latihan Punggung") },
+                    placeholder = { Text("Nama Workout", style = workoutPlanInputTextStyle) },
                     keyboardOptions = KeyboardOptions.Default.copy(
                         capitalization = KeyboardCapitalization.Words,
                         imeAction = ImeAction.Next
                     ),
-                    maxLines = 1
+                    maxLines = 1,
                 )
-                Spacer(Modifier.height(8.dp))
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-                    value = workoutNotes,
-                    onValueChange = {
-                        workoutNotes = it
-                    },
-                    label = { Text("Deskripsi") },
-                    placeholder = { Text("Ex. Fokus latihan pakai dumbell") },
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        capitalization = KeyboardCapitalization.Sentences,
-                        imeAction = ImeAction.Done
-                    ),
-                    maxLines = 1
+                Spacer(Modifier.height(24.dp))
+                HorizontalDivider(thickness = 1.dp)
+
+                var moreDetailVisible by remember { mutableStateOf(false) }
+                val rotationAnimVal by animateFloatAsState(
+                    targetValue = if (moreDetailVisible) 90F else 0F
                 )
+                Row(
+                    modifier = Modifier.clickable {
+                        moreDetailVisible = !moreDetailVisible
+                    }.fillMaxWidth().height(40.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Spacer(Modifier.width(16.dp))
+                    Text(
+                        text = "Lebih lengkap",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Icon(
+                        modifier = Modifier.size(20.dp).graphicsLayer {
+                            rotationZ = rotationAnimVal
+                        }.alpha(.75F),
+                        imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowRight,
+                        contentDescription = null,
+                    )
+                }
+                Spacer(Modifier.height(6.dp))
+                AnimatedVisibility(
+                    visible = moreDetailVisible
+                ) {
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                        value = workoutNotes,
+                        onValueChange = {
+                            workoutNotes = it
+                        },
+                        supportingText = { Text("Ex. Fokus latihan pakai dumbell") },
+                        placeholder = { Text("Deskripsi workout") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            capitalization = KeyboardCapitalization.Sentences,
+                            imeAction = ImeAction.Done
+                        ),
+                        maxLines = 1
+                    )
+                }
 
                 Spacer(Modifier.height(32.dp))
 
