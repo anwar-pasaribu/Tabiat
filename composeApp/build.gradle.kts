afa53ea3385994cb7ac220872270daea713a75bb
Anwar Pasaribu <anwar_pasaribu@live.com>
2024-06-06 23:42:52 +0700

    Version 1.4.0 (4)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -139,8 +139,8 @@ android {
         applicationId = "com.unwur.tabiatmu"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.4.0"
     }
     packaging {
         resources {
