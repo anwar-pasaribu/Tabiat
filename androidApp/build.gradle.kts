e893d7ccfcf0ba497922c75891653a984ec56af6
Anwar Pasaribu <anwar_pasaribu@live.com>
2026-07-25 01:08:09 +0700

    Bumped version to 1.8.2 (build 10) on 25 Jul 2026, 01:08am

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.unwur.tabiatmu"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 9
-        versionName = "1.8.1"
+        versionCode = 10
+        versionName = "1.8.2"
     }
     packaging {
         resources {
