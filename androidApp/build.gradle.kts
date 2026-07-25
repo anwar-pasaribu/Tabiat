aebece78bb2c087f63d08298ca62f1257b6ffa89
Anwar Pasaribu <anwar_pasaribu@live.com>
2026-07-25 03:40:52 +0000

    Bumped version to 1.8.5 (build 13) on 25 Jul 2026, 03:40am

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.unwur.tabiatmu"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "1.8.4"
+        versionCode = 13
+        versionName = "1.8.5"
     }
     packaging {
         resources {
